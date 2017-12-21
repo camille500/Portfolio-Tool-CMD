50de756e53dfb5f86508e5ff5d825e6dbe45ee4d
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-12-21 17:17:09 +0100

    Added login functionality

--- modules/account.js
+++ modules/account.js
@@ -2,17 +2,24 @@ const passwordHash = require('password-hash');
 
 const account = {
   login(req, res, next) {
+
     next();
   },
   register(req, res, next) {
-    next();
+    console.log(db.collection)
+    // const mail = req.body.mail.toLowerCase();
+    // const password = req.body.password;
+    // userCollection.findOne({'mail': mail}, function(error, results) {
+    //   console.log(results);
+    // })
+    // next();
   },
   logout(req, res, next) {
     req.session.destroy();
     next();
   },
   checkSession(req, res, next) {
-    
+
   }
 }
 
---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+(function () {
+
+  function initialize() {
+    console.log('CMD Portfolio Generator - Init');
+  };
+
+  initialize();
+
+})();
